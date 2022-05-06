84933b2a73bd0492a25ddadedf0cee01cebb7f3d
Aaron Downing <aaron.downing@bjss.com>
2022-05-06 09:52:53 +0100

    feat(cb2-3462): pr feedback - add additional test

--- tests/handler.test.ts
+++ tests/handler.test.ts
@@ -595,17 +595,88 @@ const insertEvent: DynamoDBStreamEvent = {
   }],
 };
 
+const twoTechRecordsEvent: DynamoDBStreamEvent = {
+  Records: [{
+    eventName: 'INSERT',
+    dynamodb: {
+      NewImage: {
+        systemNumber: {
+          S: '11000017',
+        },
+        'techRecord': {
+          'L': [
+            {
+              'M': {
+                'bodyType': {
+                  'M': {
+                    'code': {
+                      'S': 'r',
+                    },
+                    'description': {
+                      'S': 'refuse',
+                    },
+                  },
+                },
+                'conversionRefNo': {
+                  'S': '7891234',
+                },
+                'createdAt': {
+                  'S': '2019-06-24T10:26:54.903Z',
+                },
+                'drawbarCouplingFitted': {
+                  'BOOL': true,
+                },
+                'tyreUseCode': {
+                  'S': '2B',
+                },
+              },
+            },
+            {
+              'M': {
+                'bodyType': {
+                  'M': {
+                    'code': {
+                      'S': 'r',
+                    },
+                    'description': {
+                      'S': 'refuse',
+                    },
+                  },
+                },
+                'conversionRefNo': {
+                  'S': '7891234',
+                },
+                'createdAt': {
+                  'S': '2019-06-24T10:26:54.903Z',
+                },
+                'drawbarCouplingFitted': {
+                  'BOOL': true,
+                },
+                'tyreUseCode': {
+                  'S': '2B',
+                },
+              },
+            },
+          ],
+        }
+      }
+    }
+  }]
+}
+
 describe('handler', () => {
   beforeEach(() => {
     jest.clearAllMocks();
   });
   
   it('should not process REMOVE stream events', async () => {
-    const createRecordSpy = jest.spyOn(_, 'createTimestampRecord');
+    // @ts-ignore
+    const consoleSpy = jest.spyOn(console._stdout, 'write');
 
-    await _.handler(removeEvent).then(() => {
-      expect(createRecordSpy).toHaveBeenCalledTimes(0);
-    });
+    await _.handler(removeEvent)
+
+    expect(consoleSpy).toHaveBeenNthCalledWith(2,`info: remove event - ignoring${EOL}`)
+    expect(consoleSpy).toHaveBeenNthCalledWith(3,`info: Data processed successfully; good: 0, bad: 0${EOL}`);
   });
 
   it('should process INSERT stream events and put to dynamodb', async () => {
@@ -619,10 +690,26 @@ describe('handler', () => {
       };
     });
 
-    await _.handler(insertEvent);
-    expect(consoleSpy).toHaveBeenNthCalledWith(4, `info: Test record pushed to Dynamo${EOL}`);
-
+    await _.handler(insertEvent)
+    expect(consoleSpy).toHaveBeenNthCalledWith(2,`info: processing 1 records${EOL}`)
+    expect(consoleSpy).toHaveBeenNthCalledWith(5, `info: Data processed successfully; good: 1, bad: 0${EOL}`);
   });
+
+  it('should send two record to dynamodb when there are 2 tech records within the base tech record', async () => {
+    // @ts-ignore
+    const consoleSpy = jest.spyOn(console._stdout, 'write');
+    DynamoDB.DocumentClient.prototype.put = jest.fn().mockImplementation(() => {
+      return { 
+        promise() {
+          return Promise.resolve({});
+        },
+      };
+    });
+
+    await _.handler(twoTechRecordsEvent);
+    expect(consoleSpy).toHaveBeenNthCalledWith(2,`info: processing 2 records${EOL}`)
+    expect(consoleSpy).toHaveBeenNthCalledWith(7,`info: Data processed successfully; good: 2, bad: 0${EOL}`)
+  })
 
   it('should catch error and not put to dynamodb', async () => {
     // @ts-ignore
@@ -636,6 +723,7 @@ describe('handler', () => {
     });
     
     await _.handler(insertEvent);
+    expect(consoleSpy).toHaveBeenNthCalledWith(2,`info: processing 1 records${EOL}`)
     expect(consoleSpy).toHaveBeenNthCalledWith(5, `info: Data processed successfully; good: 0, bad: 1${EOL}`);
   });
 });
