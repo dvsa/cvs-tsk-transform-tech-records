4db659f7adef7138a70d6836a16035682ad2b397
aaron-downing <92087051+aaron-downing@users.noreply.github.com>
2023-01-25 11:25:01 +0000

    feat(cb2-7259): parallelise processing of promises when handling batch of records (#14)
    * fix(cb2-7115): allow null values to be added to flat tech records
    
    * fix(cb2-7115): refactor
    
    * fix(cb2-7115): refactor
    
    * fix(cb2-7115): more refactor
    
    * fix(cb2-7115): remove linter warning
    
    * parallel processing of promises
    
    * feat(cb2-7259): pr feedback
    
    * feat(cb2-7259): lint
    
    * extend tests

--- src/handler.ts
+++ src/handler.ts
@@ -2,48 +2,62 @@ import { DynamoDBStreamEvent } from 'aws-lambda';
 import { DynamoDB } from 'aws-sdk';
 import { createTimestampRecord } from './createTimestampRecord';
 import { LegacyVehicleRecord } from './interfaces/LegacyVehicleRecord';
+import { BatchItemFailuresResponse } from './interfaces/BatchItemFailure';
 import logger from './util/logger';
 import config from './config';
 
 const client = new DynamoDB.DocumentClient();
 
-const handler = async (event: DynamoDBStreamEvent): Promise<void> => {
+const handler = async (event: DynamoDBStreamEvent): Promise<BatchItemFailuresResponse> => {
   const sendResponse = {
     SuccessCount: 0,
     FailCount: 0,
   };
 
+  const res: BatchItemFailuresResponse = {
+    batchItemFailures: [],
+  };
+
   logger.info('Received event:', JSON.stringify(event, null, 2));
-  for (const dbRecord of event.Records) {        
-    if (dbRecord.eventName === 'REMOVE') {
-      logger.info('remove event - ignoring');
-      continue;
+  logger.info(`Received ${event.Records.length} records from technical-records DynamoDB`);
+
+  const dynamoDbPromises = event.Records.reduce((iterator, record) => {
+    if (record.eventName === 'REMOVE') {
+      logger.info('REMOVE event - ignoring');
+      return iterator;
     }
-
-    const newImage = DynamoDB.Converter.unmarshall(dbRecord.dynamodb.NewImage) as LegacyVehicleRecord;
-
+    
+    const newImage = DynamoDB.Converter.unmarshall(record.dynamodb.NewImage) as LegacyVehicleRecord;
     const techRecords = newImage.techRecord;
 
     delete newImage.techRecord;
 
-    logger.info(`processing ${techRecords.length} records`);
-    
-    for (const record of techRecords) {
-      const newRecord = createTimestampRecord(newImage, record);
-      try {
-        await (client.put({
-          TableName: config.dynamoDb.target,
-          Item: newRecord,
-        })).promise();
+    logger.info(`Processing ${techRecords.length} tech records for vehicle with systemNumber: ${newImage.systemNumber} and vin: ${newImage.vin}`);
+
+    return techRecords.map((techRecord) => {
+      const recordToSend = createTimestampRecord(newImage, techRecord);
+      return client.put({
+        TableName: config.dynamoDb.target,
+        Item: recordToSend,
+      }).promise().then(() => {
+        logger.info(`Tech record with systemNumber: ${recordToSend.systemNumber as string} and createdTimestamp: ${recordToSend.createdTimestamp as string} succesfully sent to DynamoDB`);
         sendResponse.SuccessCount++;
-        logger.info('Tech record pushed to Dynamo');
-      } catch (error) {
+      }).catch((err) => {
+        logger.error(`Tech record with systemNumber: ${recordToSend.systemNumber as string} and createdTimestamp: ${recordToSend.createdTimestamp as string} unsuccesfully sent to DynamoDB, will attempt to retry DynamoDB record`);
+        logger.error('', err);
         sendResponse.FailCount++;
-        logger.error('', error);
-      }
-    }
-  }
-  logger.info(`Data processed successfully; good: ${sendResponse.SuccessCount}, bad: ${sendResponse.FailCount}`);
+        const failureObject = { itemIdentifier: record.eventID };
+        if (res.batchItemFailures.indexOf(failureObject) === -1 ) {
+          res.batchItemFailures.push(failureObject);
+        }
+      });
+    });
+  }, [] as Promise<unknown>[]);
+
+  await Promise.allSettled(dynamoDbPromises);
+
+  logger.info(`flat-tech-records processed; succeeded: ${sendResponse.SuccessCount}, failed: ${sendResponse.FailCount}`);
+  return res;
 };
 
 export { handler };
--- tests/handler.test.ts
+++ tests/handler.test.ts
@@ -12,6 +12,7 @@ const removeEvent: DynamoDBStreamEvent = {
 
 const insertEvent: DynamoDBStreamEvent = {
   Records: [{
+    eventID: '22222',
     eventName: 'INSERT',
     dynamodb: {
       NewImage: {
@@ -55,6 +56,7 @@ const insertEvent: DynamoDBStreamEvent = {
 
 const twoTechRecordsEvent: DynamoDBStreamEvent = {
   Records: [{
+    eventID: '22222',
     eventName: 'INSERT',
     dynamodb: {
       NewImage: {
@@ -122,6 +124,38 @@ const twoTechRecordsEvent: DynamoDBStreamEvent = {
   }],
 };
 
+const twoDynamoDbRecordEvent = {
+  Records: [
+    ...twoTechRecordsEvent.Records,
+    ...twoTechRecordsEvent.Records,
+  ],
+};
+
+const removeAndInsertEvent = {
+  Records: [
+    ...removeEvent.Records,
+    ...twoTechRecordsEvent.Records,
+    ...removeEvent.Records,
+    ...twoTechRecordsEvent.Records,
+  ],
+};
+
+function returnResolvedPromise() {
+  return { 
+    promise() {
+      return Promise.resolve({});
+    },
+  };
+}
+
+function returnRejectedPromise() {
+  return { 
+    promise() {
+      return Promise.reject(new Error(''));
+    },
+  };
+}
+
 describe('handler', () => {
   beforeEach(() => {
     jest.clearAllMocks();
@@ -130,59 +164,85 @@ describe('handler', () => {
   it('should not process REMOVE stream events', async () => {
     // @ts-ignore
     const consoleSpy = jest.spyOn(console._stdout, 'write');
+    DynamoDB.DocumentClient.prototype.put = jest.fn().mockImplementation(() => returnResolvedPromise());
 
-    await handler(removeEvent);
+    const res = await handler(removeEvent);
+    expect(consoleSpy).toHaveBeenNthCalledWith(3, `info: REMOVE event - ignoring${EOL}`);
+    expect(consoleSpy).toHaveBeenNthCalledWith(4, `info: flat-tech-records processed; succeeded: 0, failed: 0${EOL}`);
+    expect(DynamoDB.DocumentClient.prototype.put).toHaveBeenCalledTimes(0);
+    expect(res).toEqual({ batchItemFailures: [] });
+  });
 
-    expect(consoleSpy).toHaveBeenNthCalledWith(2, `info: remove event - ignoring${EOL}`);
-    expect(consoleSpy).toHaveBeenNthCalledWith(3, `info: Data processed successfully; good: 0, bad: 0${EOL}`);
+  describe('when receiving REMOVE and INSERT stream events in same batch', () => {
+    it('should process INSERT event and ignore remove event', async () => {
+      // @ts-ignore
+      const consoleSpy = jest.spyOn(console._stdout, 'write');
+      DynamoDB.DocumentClient.prototype.put = jest.fn().mockImplementation(() => returnResolvedPromise());
+
+      const res = await handler(removeAndInsertEvent);
+      expect(consoleSpy).toHaveBeenNthCalledWith(3, `info: REMOVE event - ignoring${EOL}`);
+      expect(consoleSpy).toHaveBeenNthCalledWith(4, `info: Processing 2 tech records for vehicle with systemNumber: 11000017 and vin: undefined${EOL}`);
+      expect(consoleSpy).toHaveBeenNthCalledWith(7, `info: REMOVE event - ignoring${EOL}`);
+      expect(consoleSpy).toHaveBeenNthCalledWith(8, `info: Processing 2 tech records for vehicle with systemNumber: 11000017 and vin: undefined${EOL}`);
+      expect(consoleSpy).toHaveBeenNthCalledWith(15, `info: flat-tech-records processed; succeeded: 4, failed: 0${EOL}`);
+      expect(DynamoDB.DocumentClient.prototype.put).toHaveBeenCalledTimes(4);
+      expect(res).toEqual({ batchItemFailures: [] });
+    });
   });
 
   it('should process INSERT stream events and put to dynamodb', async () => {
     // @ts-ignore
     const consoleSpy = jest.spyOn(console._stdout, 'write');
-    DynamoDB.DocumentClient.prototype.put = jest.fn().mockImplementation(() => {
-      return { 
-        promise() {
-          return Promise.resolve({});
-        },
-      };
-    });
+    DynamoDB.DocumentClient.prototype.put = jest.fn().mockImplementation(() => returnResolvedPromise());
 
-    await handler(insertEvent);
-    expect(consoleSpy).toHaveBeenNthCalledWith(2, `info: processing 1 records${EOL}`);
-    expect(consoleSpy).toHaveBeenNthCalledWith(5, `info: Data processed successfully; good: 1, bad: 0${EOL}`);
+    const res = await handler(insertEvent);
+    expect(consoleSpy).toHaveBeenNthCalledWith(2, `info: Received 1 records from technical-records DynamoDB${EOL}`);
+    expect(consoleSpy).toHaveBeenNthCalledWith(5, `info: Tech record with systemNumber: 11000017 and createdTimestamp: 2019-06-24T10:26:54.903Z succesfully sent to DynamoDB${EOL}`);
+    expect(consoleSpy).toHaveBeenNthCalledWith(6, `info: flat-tech-records processed; succeeded: 1, failed: 0${EOL}`);
+    expect(DynamoDB.DocumentClient.prototype.put).toHaveBeenCalledTimes(1);
+    expect(res).toEqual({ batchItemFailures: [] });
   });
 
   it('should send two record to dynamodb when there are 2 tech records within the base tech record', async () => {
     // @ts-ignore
     const consoleSpy = jest.spyOn(console._stdout, 'write');
-    DynamoDB.DocumentClient.prototype.put = jest.fn().mockImplementation(() => {
-      return { 
-        promise() {
-          return Promise.resolve({});
-        },
-      };
-    });
+    DynamoDB.DocumentClient.prototype.put = jest.fn().mockImplementation(() => returnResolvedPromise());
 
-    await handler(twoTechRecordsEvent);
-    expect(consoleSpy).toHaveBeenNthCalledWith(2, `info: processing 2 records${EOL}`);
-    expect(consoleSpy).toHaveBeenNthCalledWith(7, `info: Data processed successfully; good: 2, bad: 0${EOL}`);
+    const res = await handler(twoTechRecordsEvent);
+    expect(consoleSpy).toHaveBeenNthCalledWith(2, `info: Received 1 records from technical-records DynamoDB${EOL}`);
+    expect(consoleSpy).toHaveBeenNthCalledWith(6, `info: Tech record with systemNumber: 11000017 and createdTimestamp: 2019-06-24T10:26:54.903Z succesfully sent to DynamoDB${EOL}`);
+    expect(consoleSpy).toHaveBeenNthCalledWith(7, `info: Tech record with systemNumber: 11000017 and createdTimestamp: 2019-06-24T10:26:54.903Z succesfully sent to DynamoDB${EOL}`);
+    expect(consoleSpy).toHaveBeenNthCalledWith(8, `info: flat-tech-records processed; succeeded: 2, failed: 0${EOL}`);
+    expect(DynamoDB.DocumentClient.prototype.put).toHaveBeenCalledTimes(2);
+    expect(res).toEqual({ batchItemFailures: [] });
   });
 
   it('should catch error and not put to dynamodb', async () => {
     // @ts-ignore
     const consoleSpy = jest.spyOn(console._stdout, 'write');
-    DynamoDB.DocumentClient.prototype.put = jest.fn().mockImplementation(() => {
-      return { 
-        promise() {
-          return Promise.reject(new Error(''));
-        },
-      };
-    });
+    DynamoDB.DocumentClient.prototype.put = jest.fn().mockImplementation(() => returnRejectedPromise());
     
-    await handler(insertEvent);
-    expect(consoleSpy).toHaveBeenNthCalledWith(2, `info: processing 1 records${EOL}`);
-    expect(consoleSpy).toHaveBeenNthCalledWith(5, `info: Data processed successfully; good: 0, bad: 1${EOL}`);
+    const res = await handler(insertEvent);
+    expect(consoleSpy).toHaveBeenNthCalledWith(2, `info: Received 1 records from technical-records DynamoDB${EOL}`);
+    expect(consoleSpy).toHaveBeenNthCalledWith(5, `error: Tech record with systemNumber: 11000017 and createdTimestamp: 2019-06-24T10:26:54.903Z unsuccesfully sent to DynamoDB, will attempt to retry DynamoDB record${EOL}`);
+    expect(consoleSpy).toHaveBeenNthCalledWith(7, `info: flat-tech-records processed; succeeded: 0, failed: 1${EOL}`);
+    expect(DynamoDB.DocumentClient.prototype.put).toHaveBeenCalledTimes(1);
+    expect(res).toEqual({ batchItemFailures: [ { itemIdentifier: '22222' } ] });
+  });
+
+  it('should send 3 records to dynamodb when lambda receives 2 dynamodb records where each contain 2 tech records but 1 flattened tech record fails to be put on dynamo', async () => {
+    // @ts-ignore
+    const consoleSpy = jest.spyOn(console._stdout, 'write');
+    DynamoDB.DocumentClient.prototype.put = jest.fn()
+      .mockImplementationOnce(() => returnResolvedPromise())
+      .mockImplementationOnce(() => returnRejectedPromise())
+      .mockImplementation(() => returnResolvedPromise());
+    
+    const res = await handler(twoDynamoDbRecordEvent);
+    expect(consoleSpy).toHaveBeenNthCalledWith(2, `info: Received 2 records from technical-records DynamoDB${EOL}`);
+    expect(consoleSpy).toHaveBeenNthCalledWith(14, `info: flat-tech-records processed; succeeded: 3, failed: 1${EOL}`);
+    expect(DynamoDB.DocumentClient.prototype.put).toHaveBeenCalledTimes(4);
+    expect(res).toEqual({ batchItemFailures: [ { itemIdentifier: '22222' } ] });
   });
 });
 
